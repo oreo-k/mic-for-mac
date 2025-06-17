8de810b14429a87c09921e6be3684965fb222dfc
Reo Kosaka <r.kosaka.titech@gmail.com>
2025-06-17 00:10:22 -0700

    Initial Commit

---
+++ mic-for-mac/mic_for_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  mic_for_macApp.swift
+//  mic-for-mac
+//
+//  Created by Reo Kosaka on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct mic_for_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mic-for-macTests/mic_for_macTests.swift
@@ -0,0 +1,17 @@
+//
+//  mic_for_macTests.swift
+//  mic-for-macTests
+//
+//  Created by Reo Kosaka on 6/17/25.
+//
+
+import Testing
+@testable import mic_for_mac
+
+struct mic_for_macTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
